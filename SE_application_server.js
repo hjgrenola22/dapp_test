ba23d1ba3b1102d4db69138d669055198a3ec018
nati <natista99@gmail.com>
2022-11-28 15:19:44 +0000

    자산 조회까지

--- SE_application_server.js
+++ SE_application_server.js
@@ -38,6 +38,23 @@ app.get("/", (req, res) => {
  const ccpPath = path.resolve(__dirname, '..', '..', '..', 'fabric-samples', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
  const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'))
 
+/* 
+
+프라이빗 네트워크에선 원래 관리자가 로그인해서 해당 사용자의 신원을 확인 후 관리자 계정으로 로그인해서 사용자 지갑을
+생성해 줘야 하는데 여기선 
+1)관리자가 지갑생성 - 지갑이 관리자 인증이고 지갑을 생성하면 네트워크 접속 허가가 생기는 거다
+2)사용자가 직접 지갑생성 - 관리자가 생성해주는 단계 생략
+3)자산 생성 - 관리자나 사용자나 지갑을 가지고 있으면 자산 생성 다 가능
+4)자산 CRUD
+
+|지갑이 그냥 로그인 할 때 사용하는 id고 지갑이 만들어지면서 인증이 되고, 네트워크에 접속 할 수 있는 허가를 받는거다 
+|자산은 로그인하면 해당 로그인당 생성할 수 있는 데이터
+
+*/
+
+//서버측에 /admin_wallet 주소 앞에 / 필수
+
+//관리자 지갑 생성
 app.post('/admin_wallet', async (req, res) => {
 
     var id = req.body.id;
@@ -64,7 +81,7 @@ app.post('/admin_wallet', async (req, res) => {
         
         //기존에 있는 wallet인지 체크
         const admin_identity = await wallet.get(id)
-        if(admin_identity){
+        if(admin_identity){//새로만드는 건 있으면 error
             console.log(`An identity for the admin user "${id}" already exists in the wallet`);
 
             var str = `An identity for the admin user "${id}" already exists in the wallet`
@@ -102,6 +119,7 @@ app.post('/admin_wallet', async (req, res) => {
     }
 })
 
+//사용자 지갑 생성
 app.post('/user_wallet', async function(req, res){
     var id = req.body.id;
     var role = req.body.role;
@@ -110,17 +128,20 @@ app.post('/user_wallet', async function(req, res){
 
     try {
         // Create a new CA client for interacting with the CA.
+        //인증서를 가져온다
         const caURL = ccp.certificateAuthorities['ca.org1.example.com'].url;
         const ca = new FabricCAServices(caURL);
 
         // Create a new file system based wallet for managing identities.
+        //지갑을 가져온다
         const walletPath = path.join(process.cwd(), 'wallet');
         const wallet = await Wallets.newFileSystemWallet(walletPath);
         console.log(`Wallet path: ${walletPath}`);
 
         // Check to see if we've already enrolled the user.
+        //user의 id 유무를 체크
         const userIdentity = await wallet.get(id);
-        if (userIdentity) {
+        if (userIdentity) { //새로만드는 건 있으면 error
             console.log(`An identity for the user "${id}" already exists in the wallet`);
 
             var str = `An identity for the user "${id}" already exists in the wallet`
@@ -131,6 +152,8 @@ app.post('/user_wallet', async function(req, res){
         }
 
         // Check to see if we've already enrolled the admin user.
+        /*관리자 아이디로 로그인해서 지갑을 생성해 줘야 하는데 그냥 "admin"으로 고정하고 사용자가 지갑 생성
+        관리자 admin 존재유무 확인*/
         const adminIdentity = await wallet.get('admin');
         if (!adminIdentity) {
             console.log('An identity for the admin user "admin" does not exist in the wallet');
@@ -143,10 +166,12 @@ app.post('/user_wallet', async function(req, res){
         }
 
         // build a user object for authenticating with the CA
+        //admin 값으로 ca에 접근해서 user 객체를 만든다
         const provider = wallet.getProviderRegistry().getProvider(adminIdentity.type);
         const adminUser = await provider.getUserContext(adminIdentity, 'admin');
 
         // Register the user, enroll the user, and import the new identity into the wallet.
+        //id와 role, adminUser값으로 secret을 만든다
         const secret = await ca.register({
             affiliation: 'org1.department1',
             enrollmentID: id,
@@ -154,7 +179,7 @@ app.post('/user_wallet', async function(req, res){
         }, adminUser);
 
         console.log("secret:",secret)
-
+        //secret으로 enroll한다. secret 값으로 user가 이후에 인증서를 재발급 받을 수 있다
         const enrollment = await ca.enroll({
             enrollmentID: id,
             enrollmentSecret: secret
@@ -167,23 +192,27 @@ app.post('/user_wallet', async function(req, res){
             mspId: 'Org1MSP',
             type: 'X.509',
         };
+        //id 이름으로 지갑 생성. 즉 지갑에 put 하는 게 생성하는 거다
         await wallet.put(id, x509Identity);
         console.log(`Successfully registered and enrolled admin user "${id}" and imported it into the wallet`);
 
+        // 결과값 출력
         var str = `Successfully registered and enrolled admin user "${id}" and imported it into the wallet`
         var jsonmsg = {'result':'SUCCESS', 'msg':str}
 
         res.status(200).json(jsonmsg)
 
     } catch (error) {
-        console.error(`Failed to register user "appUser": ${error}`);
+        console.error(`Failed to register user "${id}": ${error}`);
        
-        var str = `Failed to register user "appUser": ${error}`
+        var str = `Failed to register user "${id}": ${error}`
         var jsonmsg = {'result':'failed', 'msg':str}
 
         res.status(200).json(jsonmsg)
     }
 })
+
+/* 지갑이 그냥 id면 해당 id로 로그인했을 때 저장된 값들. 이게 자산이다 */
 
 //자산 생성
 app.post('/createasset', async (req, res) => {
@@ -196,6 +225,10 @@ app.post('/createasset', async (req, res) => {
 
     console.log(`asset-post: cert-${cert} name-${name} color-${color} size-${size} owner-${owner} appraisedValue-${value}`)
 
+    //다른 페이지로 결과를 전송하기 위해서 해당 파일을 가져온다
+    const resultPath = path.join(__dirname, "views/result.html")
+    var resultHTML = fs.readFileSync(resultPath, "utf-8")
+
     try {
         /*자산생성에서 ca연결은 필요없다. 지갑 유무로 인증을 하고
         cc에 있는 함수호출*/
@@ -206,13 +239,16 @@ app.post('/createasset', async (req, res) => {
 
         //지갑만 있으면 되기 때문에 관리자든 사용자든 상관없이 지갑 유무만 체크한다
         const userIdentity = await wallet.get(cert);
-        if (!userIdentity) {
+        if (!userIdentity) { //관리자나 사용자가 없으면 error
             console.log(`An identity for the ${cert} not exists in the wallet`);
 
-            res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"})
-            res.write("<h1>Express 서버에서 응답한 결과입니다.</h1>")
-            res.write(`<p>An identity for the ${cert} not exists in the wallet</p>`)
-            res.end()
+            var str1 = `An identity for the ${cert} not exists in the wallet`
+            var str2 = `생성자: ${cert} , 자산이름: ${name}`
+
+            resultHTML = resultHTML.replace("msgtitle", str1)
+            resultHTML = resultHTML.replace("contents", str2)
+
+            res.status(200).send(resultHTML)
 
             return;
         }
@@ -228,32 +264,112 @@ app.post('/createasset', async (req, res) => {
         const network = await gateway.getNetwork("mychannel");//채널 연결
         const contract = network.getContract("basic");//채널 코드 연결
         
-        //여기까지 ChanCode 호출 설정-------------------------------
+        //여기까지 ChainCode 호출 설정, 밑에서부터 자산 생성 --------------------------
 
-        //cc에서 CreateAsset 호출해서 자산 생성
+        //cc에서 CreateAsset 호출해서 name 이름으로 자산 생성
         await contract.submitTransaction('CreateAsset', name, color, size, owner, value);
       
         //다른 페이지로 결과 전송하기---------------------
-        const resultPath = path.jㄴoin(__dirname, "views/result.html")
-        var resultHTML = fs.readFileSync(resultPath, "utf-8")
+        var str1 = `<p>Transaction has been submiteed</P><br>`
+        var str2 = `생성자: ${cert} , 자산이름: ${name}`
 
-        str = `<p>Transaction(CreateAsset : ${name}) has been submiteed</P><br>`
+        resultHTML = resultHTML.replace("msgtitle", str1)
+        resultHTML = resultHTML.replace("contents", str2)
 
-        resultHTML = resultHTML.replace("<dir></dir>", str)
         res.status(200).send(resultHTML)
-        //다른 페이지로 결과 전송하기---------------------
+        //다른 페이지로 결과 전송하기 end ---------------------
 
+        //close(fs)
         await gateway.disconnect();
 
     } catch (error) {
         console.error(`Failed to create asset "${cert}": ${error}`);
-       
         var str = `Failed to create asset "${cert}": ${error}`
+       
+        resultHTML = resultHTML.replace("msgtitle", "실패입니다")
+        resultHTML = resultHTML.replace("contents", str)
+
+        console.log("resultHTML:"+resultHTML)
+
+        res.status(200).send(resultHTML)
+        close(fs)
+    } 
+})
+
+/*
+    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"})
+    res.write("<h1>Express 서버에서 응답한 결과입니다.</h1>")
+    res.write(`<p>An identity for the ${cert} not exists in the wallet</p>`)
+    res.end()
+
+    res 메세지는 최초 /readasset 으로 로딩했을 때 뜬다
+*/
+//자산 조회
+app.get('/readasset', async (req, res) => {
+
+    var cert = req.query.cert;//관리자나 사용자 id
+    var name = req.query.name;//asset이름
+
+    try {
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        //지갑으로 인증확인이니깐 관리자나 사용자 id
+        const identity = await wallet.get(cert);
+        if (!identity) { //관리자나 생성자가 없으면 error
+            console.log(`An identity for the "${cert}" not exist in the wallet`);
+           
+            var str = `An identity for the "${cert}" not exist in the wallet`
+            var jsonmsg = {'result':'failed', 'msg':str}
+    
+            res.status(200).json(jsonmsg)
+
+            return;
+        }
+
+        // Create a new gateway for connecting to our peer node.
+        //cert로 블록체인 네트워크 접속
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: cert, discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        //채널 연결
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        //체인코드 연결
+        const contract = network.getContract('basic');
+
+        // Evaluate the specified transaction.
+        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+        resultMsg = await contract.evaluateTransaction('ReadAsset', name);
+        //console.log(resultMsg) 주소를 받아오는 byte값
+        //console.log(`${resultMsg}`) byte값을 문자열로
+        //console.log(JSON.parse(`${resultMsg}`)) 문자열을 자바스크립트 객체로 변환
+        console.log(JSON.parse(`${resultMsg}`))
+        await gateway.disconnect();
+
+          // 결과값 출력
+        var str = `Successfully registered and enrolled admin user "${name}" and imported it into the wallet`
+        var jsonmsg = {'result':'SUCCESS', 'msg': JSON.parse(`${resultMsg}`)}
+
+        res.status(200).json(jsonmsg)
+        
+    } catch (error) {
+        console.log(`Failed to query user "${name}": ${error}`)
+
+        var str = `Failed to query user "${name}": ${error}`
         var jsonmsg = {'result':'failed', 'msg':str}
 
         res.status(200).json(jsonmsg)
+
+        return;
     }
-    
+
 })
 
 console.log(`PORT:${PORT} HOST:${HOST} is connected`)
